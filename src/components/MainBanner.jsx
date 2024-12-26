5df7f4bdad37e44db721b7eb1f04582209b512ce
Apurbo Das <akdjoy29@gmail.com>
2024-12-26 19:35:02 +0600

    slider design changed

--- src/components/MainBanner.jsx
+++ src/components/MainBanner.jsx
@@ -6,8 +6,8 @@ const MainBanner = () => {
   return (
     <div className="w-10/12 mx-auto">
       <div className="carousel w-full  h-[400px] md:h-[600px] xl:h-[800px]">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src={v1} className="w-full" />
+        <div id="slide1" className="carousel-item rounded-xl relative w-full">
+          <img src={v1} className="w-full rounded-xl" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -18,7 +18,7 @@ const MainBanner = () => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src={v2} className="w-full" />
+          <img src={v2} className="w-full rounded-xl" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -29,7 +29,7 @@ const MainBanner = () => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src={v3} className="w-full" />
+          <img src={v3} className="w-full rounded-xl" />
           <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle">
               ❮
