36277889aa437fc4a6393f147fe3dafbe00536b1
Apurbo Das <akdjoy29@gmail.com>
2025-02-12 00:55:49 +0600

    footer color changed

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/kids.gif";
 const Footer = () => {
   return (
     <div>
-      <footer className="footer bg-slate-500 text-base-content p-10 ">
+      <footer className="footer bg-slate-200 text-base-content p-10 ">
         <nav>
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Branding</a>
@@ -26,7 +26,7 @@ const Footer = () => {
           <a className="link link-hover">Cookie policy</a>
         </nav>
       </footer>
-      <footer className="footer bg-slate-500 text-base-content border-base-300 border-t px-10 py-4">
+      <footer className="footer bg-slate-200 text-base-content border-base-300 border-t px-10 py-4">
         <aside className="grid-flow-col items-center">
           <img src={logo} alt="" />
           <p>
